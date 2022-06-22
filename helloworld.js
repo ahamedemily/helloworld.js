437714c898f5c4c26b8535269670f92612e2d277
Emily Ahamed <Emily@ashleys-air-3.attlocal.net>
2022-06-22 15:40:09 -0400

    completed lab

---
+++ helloworld.js
@@ -0,0 +1,24 @@
+console.log('Hello, Universe');
+
+const firstName = "Emily";
+
+let age = 24;
+
+const isCool = true;
+console.log(typeof firstName)
+console.log(typeof age)
+console.log(typeof isCool)
+
+let x = 4;
+let y = 10;
+let z = 7 + 9;
+let sum = x + y + z;
+console.log(sum)
+
+let diff = (x+y) - (y+z)
+
+let string = 'Hello, universe'
+
+let welcomeString = string + " " + firstName // template literal, template string
+console.log(welcomeString)
+let welcomeString = `Hello, Universe ${firstName}`
